e64f121b0c816d577616b00e0103b4395edec0eb
Idowu Emehinola <hydeenoble39@gmail.com>
2019-09-13 21:54:33 +0100

    Added contact section to contact page

--- src/contact/index.js
+++ src/contact/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Nav from '../components/nav'
 import Header from './header'
-
+import ContactSection from "./contact";
 
 function Contact() {
     return (
         <div className="body_wrapper">
             <Nav />
             <Header/>
+            <ContactSection />
         </div>
     );
 }
