22c5310f0946af3efa3337806e8d337075ff339a
Idowu Emehinola <hydeenoble39@gmail.com>
2019-09-25 22:35:52 +0100

    Restructured content on meetup page

--- src/meetup/why-meetup.js
+++ src/meetup/why-meetup.js
@@ -19,7 +19,10 @@ function WhyMeetUp() {
                         <div className="p_service_item pr_70">
                             <div className="icon icon_one"><i className="ti-panel"></i></div>
                             <h5 className="f_600 f_p t_color3">Continuous Learning</h5>
-                            <p className="f_400">Part of the benefit of belonging to a community is the open learning culture that we have, where engineers from different organizations come and share knowledge and ideas on how to grow as a person and also how to help your organization, also you can always bring your technical and business problem to the community and you can be sure that there is always someone that has faced the challenge before.</p>
+                            <p className="f_400">
+                                Part of the benefit of belonging to a community is the open learning culture that we have, where engineers from different organizations come and share knowledge and ideas on how to grow as a person.
+                                {/*and also how to help your organization, also you can always bring your technical and business problem to the community and you can be sure that there is always someone that has faced the challenge before.*/}
+                            </p>
                         </div>
                     </div>
                     <div className="col-lg-4 col-sm-6">
@@ -33,7 +36,9 @@ function WhyMeetUp() {
                         <div className="p_service_item pl_70">
                             <div className="icon icon_three"><i className="ti-fullscreen"></i></div>
                             <h5 className="f_600 f_p t_color3">Giving back to the community</h5>
-                            <p className="f_400">One of the reasons why we started DevOps Nigeria is because we want to give back to the community and one of the ways we have been able to achieve that is by teaching and mentoring other junior and upcoming engineers that would like to become a DevOps, Cloud or Site Reliability Engineer in achieving their dream.</p>
+                            <p className="f_400">One of the reasons started DevOps Nigeria was given birth to is the fact that we want to give back to the community and one of the ways we have been able to achieve that is by <strong>Mentoring</strong>
+                                {/*teaching and mentoring other junior and upcoming engineers that would like to become a DevOps, Cloud or Site Reliability Engineer in achieving their dream.*/}
+                            </p>
                         </div>
                     </div>
                     {/* <div className="col-lg-4 col-sm-6">
