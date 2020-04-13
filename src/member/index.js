e82bd1efca0e8bdc67b357d03cc6bfe3e26cc9f8
hydeenoble <hydeenoble39@gmail.com>
2020-04-13 22:52:40 +0100

    added structure for members page

--- src/member/index.js
+++ src/member/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../components/nav";
 import Header from "./header";
+import Sponsors from "../components/sponsors";
 import Footer from "../components/footer";
 
 function Member() {
